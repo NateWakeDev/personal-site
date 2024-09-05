ae1562f8c857613ebb2f0526d0e9eb5f5048eb00
Nate Wakefield <beachkid282@gmail.com>
2024-09-05 02:20:38 -0700

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,23 +1,41 @@
-// src/app/components/Header.js
+"use client";
+
 import Link from 'next/link';
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
 
 const Header = () => {
   return (
-    <nav className="bg-gray-800 p-4 mb-4">
+    <nav className="bg-gray-800 p-2 mb-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-lg font-bold">
-          Nathan Wakefield
+        <Link href="/" className="text-white text-lg font-bold flex items-center">
+          <img src="/favicon.ico" alt="favicon" className="w-10 h-10 mr-2 inline-block" />
+          <span>Nathan Wakefield</span>
         </Link>
         <div>
-          <Link href="/about" className="text-white mr-4">
-            About
-          </Link>
-          <Link href="/projects" className="text-white mr-4">
-            Projects
-          </Link>
-          <Link href="/contact" className="text-white">
-            Contact
-          </Link>
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="bordered" className="accordion-border items-center">
+                Open Menu
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              <DropdownItem key="About" className="accordion-border">
+                <Link href="/about" className="text-white items-center">
+                  About
+                </Link>
+              </DropdownItem>
+              <DropdownItem key="Projects" className="accordion-border">
+                <Link href="/projects" className="text-white items-center">
+                  Projects
+                </Link>
+              </DropdownItem>
+              <DropdownItem key="Contact" className="accordion-border items-center">
+                <Link href="/contact" className="text-white items-center">
+                  Contact
+                </Link>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </div>
       </div>
     </nav>
